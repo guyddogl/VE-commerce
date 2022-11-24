01cfbbb9eabd473f15f6b13d72477bb3ae80ed67
guyddogl <guyddogl@gmail.com>
2022-11-24 10:18:23 -0300

    feat: Configura as rotas Home, Login e NotFound

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import './assets/css/global.css';
+import Login from './pages/Login';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div>
-      Hello, VE-commerce
-    </div>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/" element={<Home />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 }
 
