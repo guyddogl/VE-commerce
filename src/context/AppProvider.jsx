8059ea6a24780db22568ccdf9c46fb234249506a
guyddogl <guyddogl@gmail.com>
2022-11-24 11:04:41 -0300

    feat: Cria o estado global da aplicação

---
+++ src/context/AppProvider.jsx
@@ -0,0 +1,26 @@
+import React, { useState, useMemo } from 'react';
+import PropTypes from 'prop-types';
+import AppContext from './AppContext';
+
+export default function AppProvider({ children }) {
+  const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
+
+  const [currentUser, setCurrentUser] = useState(null);
+
+  const contextValue = useMemo(() => ({
+    isUserLoggedIn,
+    setIsUserLoggedIn,
+    currentUser,
+    setCurrentUser,
+  }));
+
+  return (
+    <AppContext.Provider value={contextValue}>
+      {children}
+    </AppContext.Provider>
+  );
+}
+
+AppProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
