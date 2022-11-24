9512edb71c043de6ef509558a2ae3d4bb0dd5047
guyddogl <guyddogl@gmail.com>
2022-11-24 11:47:07 -0300

    feat: Cria a navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,58 @@
+import React, { useContext } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import logo from '../assets/img/logo.png';
+import AppContext from '../context/AppContext';
+
+export default function NavBar() {
+  const { currentUser, setCurrentUser, setIsUserLoggedIn } = useContext(AppContext);
+
+  const location = useLocation();
+
+  const navigate = useNavigate();
+
+  const logUserOut = () => {
+    setCurrentUser(null);
+    setIsUserLoggedIn(false);
+    navigate('/login');
+  };
+
+  const checkNavLinkActive = (nav, path) => (nav === path ? 'nav-link-active' : '');
+
+  return (
+    <nav className="navbar navbar-expand-lg bg-light border-bottom">
+      <div className="container">
+        <Link className="navbar-brand" to="/">
+          <img src={logo} alt="Logo" width="42" height="42" className="d-inline-block align-text-top" />
+        </Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item mx-1">
+              <Link className={`nav-link ${checkNavLinkActive('/', location.pathname)}`} to="/">Home</Link>
+            </li>
+            <li className="nav-item mx-1">
+              <Link className={`nav-link ${checkNavLinkActive('/produto/add', location.pathname)}`} to="/produto/add">Adicionar Produto</Link>
+            </li>
+            <li className="nav-item mx-1">
+              <Link className={`nav-link ${checkNavLinkActive('/relatorios', location.pathname)}`} to="/">Relatórios</Link>
+            </li>
+          </ul>
+          <span className="navbar-text">
+            Olá,
+            {' '}
+            <b>{currentUser}</b>
+          </span>
+          <button
+            type="button"
+            className="btn btn-sm btn-outline-danger ms-2"
+            onClick={logUserOut}
+          >
+            <i className="fa-solid fa-right-from-bracket" />
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+}
