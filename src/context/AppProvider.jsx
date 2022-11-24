1cee70686d002809e0125a6e7c562c10ac93fc09
guyddogl <guyddogl@gmail.com>
2022-11-24 11:54:38 -0300

    feat: Armaneza a lista de produtos no estado global

--- src/context/AppProvider.jsx
+++ src/context/AppProvider.jsx
@@ -1,17 +1,34 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import AppContext from './AppContext';
+import { getAllProducts } from '../services/fetchApi';
 
 export default function AppProvider({ children }) {
   const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
 
   const [currentUser, setCurrentUser] = useState(null);
 
+  const [allProducts, setAllProducts] = useState([]);
+
+  const [fetchLoading, setFetchLoading] = useState(false);
+
+  useEffect(() => {
+    setFetchLoading(true);
+    const getProducts = async () => {
+      const products = await getAllProducts();
+      setFetchLoading(false);
+      setAllProducts(products.products);
+    };
+    getProducts();
+  }, []);
+
   const contextValue = useMemo(() => ({
     isUserLoggedIn,
     setIsUserLoggedIn,
     currentUser,
     setCurrentUser,
+    allProducts,
+    fetchLoading,
   }));
 
   return (
