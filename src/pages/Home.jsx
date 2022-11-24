876f2b3246bf0a0eaad49a6d695f0475350abf2e
guyddogl <guyddogl@gmail.com>
2022-11-24 12:53:11 -0300

    feat: Cria botões para exibir mais ou menos produtos

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NavBar from '../components/NavBar';
 import ListOfProducts from '../components/ListOfProducts';
 
 export default function Home() {
+  const [limitOfProducts, setLimitOfProducts] = useState(8);
   return (
     <>
       <NavBar />
       <section className="container mt-4">
-        <ListOfProducts limit={10} />
+        <ListOfProducts limit={limitOfProducts} />
+        <div className="row mt-3 justify-content-center text-center">
+          <div className="col-10 col-lg-4">
+            {limitOfProducts === 8 && (
+            <button
+              type="button"
+              onClick={() => setLimitOfProducts(30)}
+              className="btn btn-sm btn-secondary"
+            >
+              <i className="fa-solid fa-plus me-2" />
+              Exibir mais produtos
+            </button>
+            )}
+            {limitOfProducts === 30 && (
+            <button
+              type="button"
+              onClick={() => setLimitOfProducts(8)}
+              className="btn btn-sm btn-secondary"
+            >
+              <i className="fa-solid fa-minus me-2" />
+              Exibir menos produtos
+            </button>
+            )}
+          </div>
+        </div>
       </section>
     </>
   );
