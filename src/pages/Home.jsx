3e58b9ef4db6494791c210eed58d73ba1096111a
guyddogl <guyddogl@gmail.com>
2022-11-24 12:52:02 -0300

    feat: Exibe a lista de produtos

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
+import ListOfProducts from '../components/ListOfProducts';
 
 export default function Home() {
   return (
     <>
       <NavBar />
-      Home
+      <section className="container mt-4">
+        <ListOfProducts limit={10} />
+      </section>
     </>
   );
 }
