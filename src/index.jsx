42809fe16dab1b1c9daf3b06e8e16356c8688250
guyddogl <guyddogl@gmail.com>
2022-11-24 11:07:18 -0300

    feat: importa o estado global

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import AppProvider from './context/AppProvider';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter basename="/">
-    <App />
+    <AppProvider>
+      <App />
+    </AppProvider>
   </BrowserRouter>,
 );
