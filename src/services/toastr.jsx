cc8de1330911a2c3a0fe1069d4a183c025855b94
guyddogl <guyddogl@gmail.com>
2022-11-25 19:57:57 -0300

    refactor: Altera extensão para jsx

---
+++ src/services/toastr.jsx
@@ -0,0 +1,26 @@
+import toastr from 'toastr';
+
+const toastTopRight = {
+  closeButton: true,
+  debug: false,
+  newestOnTop: false,
+  progressBar: true,
+  positionClass: 'toast-top-center',
+  preventDuplicates: false,
+  onclick: null,
+  showDuration: '300',
+  hideDuration: '1000',
+  timeOut: '3000',
+  extendedTimeOut: '1000',
+  showEasing: 'swing',
+  hideEasing: 'linear',
+  showMethod: 'slideDown',
+  hideMethod: 'slideUp',
+};
+
+export default function showToast(type, message) {
+  toastr.options = toastTopRight;
+  if (type === 'success') return toastr.success(message);
+  if (type === 'error') return toastr.error(message);
+  return false;
+}
