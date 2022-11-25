83fc6a6bca290f0cfbf1b2fb3565b15ebd29e7f0
guyddogl <guyddogl@gmail.com>
2022-11-25 15:47:38 -0300

    feat: Exibi ou não os textos dos botões conforme o tamanho da tela do dispositivo do cliente

--- src/components/ListOfProducts.jsx
+++ src/components/ListOfProducts.jsx
@@ -1,9 +1,8 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useNavigate } from 'react-router-dom';
-import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
-import Tooltip from 'react-bootstrap/Tooltip';
 import AppContext from '../context/AppContext';
+import ModalDeleteProduct from './ModalDeleteProduct';
 
 export default function ListOfProducts(props) {
   const { allProducts, fetchLoading } = useContext(AppContext);
@@ -13,6 +12,8 @@ export default function ListOfProducts(props) {
   const navigate = useNavigate();
 
   const [inputSearch, setInputSearch] = useState('');
+
+  const [deleteProduct, setDeleteProduct] = useState({});
 
   const seeProduct = (id) => {
     const route = `/product/${id}`;
@@ -24,15 +25,27 @@ export default function ListOfProducts(props) {
     return navigate(route);
   };
 
-  const deleteProduct = (product) => {
-    console.log(product);
+  const [show, setShow] = useState(false);
+
+  const handleShow = () => setShow(true);
+
+  const handleClose = () => {
+    setDeleteProduct({});
+    setShow(false);
   };
+
+  useEffect(() => {
+    if (Object.keys(deleteProduct).length > 0) {
+      handleShow();
+    }
+  }, [deleteProduct]);
 
   const lista = allProducts
     .filter((product) => product.title.toLowerCase().includes(inputSearch.toLowerCase()));
 
   return (
     <>
+      <ModalDeleteProduct show={show} product={deleteProduct} handleClose={handleClose} />
       <div className="col-6 col-lg-4">
         <label htmlFor="nome" className="form-label" style={{ width: '100%' }}>
           <div className="input-group">
@@ -50,7 +63,6 @@ export default function ListOfProducts(props) {
           </div>
         </label>
       </div>
-
       <ul className="list-group list-group-flush mt-2">
         <li className="list-group-item bg-light text-dark">
           Lista de Produtos
@@ -68,7 +80,7 @@ export default function ListOfProducts(props) {
               <div className="col-2 col-lg-1">
                 <img src={product.thumbnail} alt={product.title} className="img-fluid mx-auto d-block img-thumbnail" style={{ maxHeight: '80px' }} />
               </div>
-              <div className="col-8 col-lg-8">
+              <div className="col-8 col-lg-7">
                 <p>
                   <b>{product.title}</b>
                   {' '}
@@ -77,46 +89,34 @@ export default function ListOfProducts(props) {
                   {`${product.description.substr(0, 100)}...`}
                 </p>
               </div>
-              <div className="col-2 col-lg-3 text-end">
-                <OverlayTrigger
-                  placement="top"
-                  overlay={(<Tooltip>Ver detalhes</Tooltip>)}
+              <div className="col-2 col-lg-4 text-end">
+                <button
+                  type="button"
+                  className={`btn btn-md btn-outline-primary m-1 ${fetchLoading && 'disabled'}`}
+                  style={{ minWidth: '45px' }}
+                  onClick={() => seeProduct(product.id)}
                 >
-                  <button
-                    type="button"
-                    className="btn btn-md btn-outline-primary m-1"
-                    style={{ minWidth: '45px' }}
-                    onClick={() => seeProduct(product.id)}
-                  >
-                    <i className="fa-solid fa-file-lines" />
-                  </button>
-                </OverlayTrigger>
-                <OverlayTrigger
-                  placement="top"
-                  overlay={(<Tooltip>Editar</Tooltip>)}
+                  <i className="fa-solid fa-file-lines me-lg-2" />
+                  <span className="d-none d-lg-inline">Visualizar</span>
+                </button>
+                <button
+                  type="button"
+                  className={`btn btn-md btn-outline-secondary m-1 ${fetchLoading && 'disabled'}`}
+                  style={{ minWidth: '45px' }}
+                  onClick={() => editProduct(product.id)}
                 >
-                  <button
-                    type="button"
-                    className="btn btn-md btn-outline-secondary m-1"
-                    style={{ minWidth: '45px' }}
-                    onClick={() => editProduct(product.id)}
-                  >
-                    <i className="fa-regular fa-pen-to-square" />
-                  </button>
-                </OverlayTrigger>
-                <OverlayTrigger
-                  placement="top"
-                  overlay={(<Tooltip>Excluir</Tooltip>)}
+                  <i className="fa-regular fa-pen-to-square me-lg-2" />
+                  <span className="d-none d-lg-inline">Editar</span>
+                </button>
+                <button
+                  type="button"
+                  className={`btn btn-md btn-outline-danger m-1 ${fetchLoading && 'disabled'}`}
+                  style={{ minWidth: '45px' }}
+                  onClick={() => setDeleteProduct(product)}
                 >
-                  <button
-                    type="button"
-                    className="btn btn-md btn-outline-danger m-1"
-                    style={{ minWidth: '45px' }}
-                    onClick={() => deleteProduct(product)}
-                  >
-                    <i className="fa-solid fa-trash" />
-                  </button>
-                </OverlayTrigger>
+                  <i className="fa-solid fa-trash me-lg-2" />
+                  <span className="d-none d-lg-inline">Excluir</span>
+                </button>
               </div>
             </div>
           </li>
