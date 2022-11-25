22e6cc680338c6e41958c49fbc32207a27922fb5
guyddogl <guyddogl@gmail.com>
2022-11-25 09:12:54 -0300

    feat: Cria requisição para buscar o dados do produto de acordo com o id da rota

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { useParams } from 'react-router-dom';
 import NavBar from '../components/NavBar';
+import { getProductById } from '../services/fetchApi';
+import AppContext from '../context/AppContext';
 
 export default function ProductDetails() {
+  const { fetchLoading, setFetchLoading } = useContext(AppContext);
+
+  const [product, setProduct] = useState({});
+
   const { id } = useParams();
+
+  useEffect(() => {
+    setFetchLoading(true);
+    const getProduct = async () => {
+      await getProductById(id).then((response) => setProduct(response));
+      setFetchLoading(false);
+    };
+    getProduct();
+  }, []);
 
   return (
     <>
@@ -12,6 +27,12 @@ export default function ProductDetails() {
         Product Detail
         {' '}
         {id}
+        {fetchLoading && (
+          <div className="spinner-border spinner-border-sm text-secondary ms-2" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </div>
+        )}
+        {product.title}
       </section>
     </>
   );
