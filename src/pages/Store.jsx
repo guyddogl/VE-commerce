d7bf96ba1830a0cd2a91ed1f2f23f69c02667d10
guyddogl <guyddogl@gmail.com>
2022-11-26 12:25:28 -0300

    feat: Página para exibição dos produtos

--- src/pages/Store.jsx
+++ src/pages/Store.jsx
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import NavBar from '../components/NavBar';
+import ProductCard from '../components/ProductCard';
+import AppContext from '../context/AppContext';
 
 export default function Store() {
+  const { allProducts, fetchLoading } = useContext(AppContext);
+
+  console.log(allProducts);
+  // {allProducts.length > 0 && allProducts[0].title}
   return (
-    <NavBar />
+    <>
+      <NavBar />
+      <section className="container mt-4">
+        {fetchLoading ? (
+          <div className="row justify-content-center my-5">
+            <div className="spinner-border spinner-border-lg text-primary" />
+          </div>
+        ) : (
+          <div className="album my-5">
+            <div className="container">
+              <div className="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-3 g-4">
+                {allProducts.length > 0
+              && allProducts.map((product) => (
+                <ProductCard key={product.title} product={product} />
+              ))}
+              </div>
+            </div>
+          </div>
+        )}
+      </section>
+    </>
   );
 }
